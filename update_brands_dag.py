12861272d0e6878228d5b1956ff7fea691c22687
admin <admin@artyoms-mbp.lan>
2022-12-06 12:16:40 +0400

    add patients

--- update_brands_dag.py
+++ update_brands_dag.py
@@ -253,6 +253,70 @@ def upsert_patient_group(ti):
             cursor.execute(query)
 
 
+def upsert_patients(ti):
+    customers = ti.xcom_pull(task_ids=['get_customers'])
+    if not customers:
+        raise Exception('No customers.')
+    else:
+        for customer in customers:
+            comp_id = customer[0]
+            db_name = customer[1]
+            ext_schema = f'ext_indica_{db_name}'
+            redshift_hook = RedshiftSQLHook(
+                postgres_conn_id='redshift_default',
+                schema='dev'
+            )
+            redshift_conn = redshift_hook.get_conn()
+            cursor = redshift_conn.cursor()
+            query = f'''
+                CREATE temporary TABLE patients_{comp_id}_temp as
+                SELECT *
+                FROM {ext_schema}.patients
+                WHERE sync_updated_at > (
+                    SELECT coalesce(max(sync_updated_at), '1970-01-01 00:00:00'::timestamp)
+                    FROM staging.patients
+                    WHERE comp_id = {comp_id}
+                )
+            '''
+            cursor.execute(query)
+            query = f'''
+                DELETE FROM staging.patients
+                USING patients_{comp_id}_temp
+                WHERE staging.patients.comp_id = {comp_id}
+                    AND staging.patients.id = patients_{comp_id}_temp.id
+            '''
+            cursor.execute(query)
+            query = f'''
+                INSERT INTO staging.patients
+                SELECT {comp_id}, pat_id, pat_user_id, pat_user_info, pat_office_id, pat_first_name, pat_middle_name, pat_last_name, 
+                    pat_gender, pat_photo, pat_dob, pat_phone, pat_alt_phone, pat_email, pat_state_name, pat_city_name, pat_zip_name, 
+                    pat_address1, pat_address2, pat_dmv, pat_dhc, pat_passport, pat_notes, pat_ref_points, pat_is_hidden, pat_is_disabled, 
+                    pat_is_veteran, pat_is_military, pat_is_seniou, pat_is_nonpatient, pat_is_facebooked, pat_is_needs_cultivation, 
+                    pat_is_cancer, pat_status, pat_weight, pat_height, pat_amount, pat_created_at_date, pat_is_deleted, pat_deleted_at, 
+                    pat_deleted_by_doc_id, pat_stamp, pat_is_appointment, pat_last_visit_date, is_crypted, pat_skype, pat_insurance, 
+                    pat_general_intake, pat_has_intake_for_approving, 
+                    cast(is_not_send_mail as BOOLEAN) as is_not_send_mail, 
+                    cast(is_not_send_email as BOOLEAN) as is_not_send_email, 
+                    cast(is_not_send_sms as BOOLEAN) as is_not_send_sms, 
+                    cast(is_not_call as BOOLEAN) as is_not_call, 
+                    unsubscribe_hash, is_from_shop, pat_ssn, pat_ethnicity, pat_race, pat_address_lon, pat_address_lat, pat_m_phone1, 
+                    pat_m_phone2, pat_m_phone3, pat_h_phone1, pat_h_phone2, pat_h_phone3, pat_o_phone1, pat_o_phone2, pat_o_phone3, pat_fax, 
+                    pat_lng, current_rec_valid_to, current_rec_doc_id, current_rec_number, sync_updated_at, sync_created_at, pat_is_ssi, 
+                    pat_is_compassion, pat_is_vip, pat_is_staff, pat_is_former_staff, pat_is_markedasdeleted, pat_markedasdeleted_reason, 
+                    pat_markedasdeleted_username, pat_markedasdeleted_date, balance, pat_county_name, pat_location_id, pat_state_id, pat_county_id, 
+                    pat_city_id, pat_zip_id, is_typist_work, pat_dmv_alt, pat_is_blacklist, pat_blacklist_reason, "type", pat_phone_twilio_lookup, 
+                    pat_alt_phone_twilio_lookup, is_twilio_lookup, agile_crm_id, potify_id, potify_earned_cashback, potify_spent_cashback, created_at, 
+                    updated_at, is_tax_exempt, tax_profile_id, phone_is_consented, email_is_consented, phone_consent_given_at, 
+                    phone_consent_signature, email_consent_given_at, email_consent_signature, deleted_at, tax_tier_id, buyer_status, marketplace
+                FROM patients_{comp_id}_temp
+            '''
+            cursor.execute(query)
+            query = f'''
+                DROP TABLE patients_{comp_id}_temp
+            '''
+            cursor.execute(query)
+
+
 with DAG(
     dag_id='update_brands_dag',
     schedule_interval='@daily',
@@ -284,13 +348,18 @@ with DAG(
         task_id='upsert_patient_group',
         python_callable=upsert_patient_group
     )
+    task_upsert_patients = PythonOperator(
+        task_id='upsert_patients',
+        python_callable=upsert_patients
+    )
 
     task_get_customers >> [
         task_upsert_brands, 
         task_upsert_company_config, 
         task_upsert_discounts, 
         task_upsert_patient_group_ref,
-        task_upsert_patient_group
+        task_upsert_patient_group,
+        upsert_patients
     ]
 
 
