142cad4fa86d396683034f0df8fc2b8121b1e688
admin <admin@artyoms-mbp.lan>
2022-12-08 12:06:39 +0400

    fix comma 2

--- test_dbt.py
+++ test_dbt.py
@@ -3,15 +3,13 @@ from datetime import timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import datetime
-from airflow.utils.dates import timedelta
 
 
 with DAG(
-    dag_id='dbt_dag',
-    start_date=datetime(year=2022, month=12, day=8),
+    dag_id='update_staging',
     schedule_interval='0 9 * * *', # UTC time
-    description='An Airflow DAG to invoke simple dbt commands',
-    schedule_interval=timedelta(days=1)
+    start_date=datetime(year=2022, month=12, day=8),
+    catchup=False,
 ) as dag:
 
     activate = BashOperator(
