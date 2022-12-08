4e0fb42d851ef46d661e053b9ec278fc0b8f5cd6
admin <admin@artyoms-mbp.lan>
2022-12-08 12:03:14 +0400

    add activate and deactivate

--- test_dbt.py
+++ test_dbt.py
@@ -8,19 +8,30 @@ from airflow.utils.dates import timedelta
 
 with DAG(
     dag_id='dbt_dag',
-    start_date=datetime(2021, 12, 23),
+    start_date=datetime(year=2022, month=12, day=8),
+    schedule_interval='0 9 * * *', # UTC time
     description='An Airflow DAG to invoke simple dbt commands',
     schedule_interval=timedelta(days=1),
 ) as dag:
 
-    dbt_run = BashOperator(
-        task_id='dbt_run',
-        bash_command='dbt run'
+    activate = BashOperator(
+        task_id='activate',
+        bash_command='source ~/dbt/venv/bin/activate; cd ~/dbt/indica;'
     )
+
+    # dbt_run = BashOperator(
+    #     task_id='dbt_run',
+    #     bash_command='dbt run'
+    # )
 
     dbt_test = BashOperator(
         task_id='dbt_test',
         bash_command='dbt test'
     )
 
-    dbt_run >> dbt_test
+    deactivate = BashOperator(
+        task_id='activate_venv',
+        bash_command='deactivate'
+    )
+
+    activate >> dbt_test >> deactivate
