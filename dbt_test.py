bc30223017e09e733a9f798f7ee4fa2443e7d907
admin <admin@artyoms-mbp.lan>
2023-01-10 10:53:34 +0400

    add project_dir and profiles_dir

--- dbt_test.py
+++ dbt_test.py
@@ -18,11 +18,22 @@ with DAG(
 ) as dag:
     dbt_test = DbtTestOperator(
         task_id="dbt_test",
+        project_dir="/home/ubuntu/dbt/indica",
+        profiles_dir="/home/ubuntu/.dbt",
     )
     dbt_run = DbtRunOperator(
         task_id="dbt_run",
-        select=["~/dbt/indica/models"],
+        project_dir="/home/ubuntu/dbt/indica",
+        profiles_dir="/home/ubuntu/.dbt",
         # fail_fast=True,
+        # task_id="dbt_run_hourly",
+        # project_dir="s3://my-bucket/dbt/project/key/prefix/",
+        # profiles_dir="s3://my-bucket/dbt/profiles/key/prefix/",
+        # select=["+tag:hourly"],
+        # exclude=["tag:deprecated"],
+        # target="production",
+        # profile="my-project",
+        # full_refresh=False,
     )
 
     dbt_run >> dbt_test
