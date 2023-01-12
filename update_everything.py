9c53d828679b42fb8222cd25ca288682a4d1c862
denis-prk <d.prokopev@telemedicine.ge>
2023-01-12 14:34:53 +0400

    add task for product_checkins table

--- update_everything.py
+++ update_everything.py
@@ -909,6 +909,61 @@ def upsert_service_history(customers):
             logging.info(f'Task is finished for company {comp_id}')
 
 
+def upsert_product_checkins(customers):
+    if not customers:
+        raise Exception('No customers found')
+    else:
+        redshift_hook = RedshiftSQLHook(
+                postgres_conn_id='redshift_default',
+                schema='dev'
+            )
+        redshift_conn = redshift_hook.get_conn()
+        for comp_id, ext_schema in customers:
+            logging.info(f'Task is starting for company {comp_id}')
+            with redshift_conn.cursor() as cursor:
+                query = f'''
+                    CREATE temporary TABLE product_checkins_{comp_id}_temp as
+                    SELECT *
+                    FROM {ext_schema}.product_checkins
+                    WHERE sync_updated_at > (
+                        SELECT coalesce(max(sync_updated_at), '1970-01-01 00:00:00'::timestamp)
+                        FROM staging.product_checkins
+                        WHERE comp_id = {comp_id}
+                    ) and sync_updated_at < CURRENT_DATE + interval '8 hours'
+                '''
+                cursor.execute(query)
+                logging.info(f'Temp table is created')
+            with redshift_conn.cursor() as cursor:
+                query = f'''
+                    DELETE FROM staging.product_checkins
+                    USING product_checkins_{comp_id}_temp
+                    WHERE staging.product_checkins.comp_id = {comp_id}
+                        AND staging.product_checkins.id = product_checkins_{comp_id}_temp.id
+                '''
+                cursor.execute(query)
+                logging.info(f'{cursor.rowcount} rows deleted for {comp_id} at {datetime.now()}')
+            with redshift_conn.cursor() as cursor:
+                query = f'''
+                    INSERT INTO staging.product_checkins
+                    SELECT {comp_id}, id, product_id, vendor_id, user_id, qty, price, date, status, balance, note, batch_id, lab_result_id, has_lab_result, uid,
+                    harvest_date, sync_created_at, sync_updated_at, opc, sale_qty, office_id, is_metrc, available_qty, is_finished, producer_id, 
+                    vendor_type, vendor_name, facility_id, is_special, packaged_date, best_by_date, deleted_at, production_run, is_under_package_control, 
+                    is_form_modified, metrc_initial_quantity, external_barcode, packaged_by_id, manifest, is_sample_package, paused_to_datetime, 
+                    excise_tax_paid, link_to_coa
+                    FROM product_checkins_{comp_id}_temp
+                '''
+                cursor.execute(query)
+                logging.info(f'{cursor.rowcount} rows inserted for {comp_id} at {datetime.now()}')
+            with redshift_conn.cursor() as cursor:
+                query = f'''
+                    DROP TABLE product_checkins_{comp_id}_temp
+                '''
+                cursor.execute(query)
+                logging.info(f'Temp table is dropped')
+            redshift_conn.commit()
+            logging.info(f'Task is finished for company {comp_id}')
+
+
 customers = get_customers()
 
 
@@ -1006,6 +1061,11 @@ with DAG(
     task_upsert_warehouse_order_items = PythonOperator(
         task_id='upsert_warehouse_order_items',
         python_callable=upsert_warehouse_order_items,
+        op_args=[customers]
+    )
+    task_upsert_upsert_product_checkins = PythonOperator(
+        task_id='upsert_product_checkins',
+        python_callable=upsert_product_checkins,
         op_args=[customers]
     )
     dbt_run = DbtRunOperator(
