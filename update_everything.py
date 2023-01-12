f375c47868260a0271fb4e6eb252e4462dc8d507
admin <admin@artyoms-mbp.lan>
2023-01-12 17:40:52 +0400

    replace companies table with int_customers view

--- update_everything.py
+++ update_everything.py
@@ -27,31 +27,11 @@ def get_customers():
     redshift_conn = redshift_hook.get_conn()
     cursor = redshift_conn.cursor()
     query = '''
-        SELECT companies.comp_id, TRIM(svv_external_schemas.schemaname) as schemaname
-        FROM ext_indica_backend.companies
+        SELECT int_customers.comp_id, TRIM(svv_external_schemas.schemaname) as schemaname
+        FROM test.int_customers
         INNER JOIN svv_external_schemas
-        ON companies.db_name = svv_external_schemas.databasename
-        WHERE 1=1
-            and (db_name like '%_company'
-	            and is_blank = 0
-	            and comp_project = 'Indica'
-	            and not domain_prefix like '%prod'
-	            and not domain_prefix like 'test%'
-	            and not domain_prefix like '%demo%'
-	            and not comp_email like '%maildrop.cc'
-	            and not comp_email like '%indica%'
-	            and not comp_name like 'Blank company%'
-	            and not comp_name like '%test%'
-	            and not comp_name like '%Test%'
-	            and not comp_name like '%xxxx%'
-	            and plan <> 5
-	            and comp_id not in (8580, 724, 6805, 8581, 6934, 8584, 
-	                8585, 3324, 8582, 6022, 3439, 8583, 8586, 6443, 8588, 
-	                6483, 7900, 8587, 8589, 9471, 7304, 7523, 8911, 213
-	            ) and potify_sync_entity_updated_at >= current_date - interval '1 day'
-	            and comp_is_approved = 1
-	        )
-            or (comp_id in (10461, 9868))
+        ON int_customers.db_name = svv_external_schemas.databasename
+        WHERE int_customers.potify_sync_entity_updated_at >= current_date - interval '1 day'
         ORDER BY comp_id
     '''
     cursor.execute(query)
