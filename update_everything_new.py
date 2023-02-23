2ab4a7f5cb9a2d9eeba1c219686a86dfd5472983
Igor Plotnikov <i.plotnikov@telemedicine.ge>
2023-02-23 12:13:43 +0400

    replace now() with current_timestamp

--- update_everything_new.py
+++ update_everything_new.py
@@ -146,7 +146,7 @@ def upsert_brands(schema, table, date_column, **kwargs):
         # inserting increment to target table
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id}, id, brand_id, brand_name, wm_id, sync_created_at, sync_updated_at, description, is_internal, now() as inserted_at
+            SELECT {comp_id}, id, brand_id, brand_name, wm_id, sync_created_at, sync_updated_at, description, is_internal, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -184,7 +184,7 @@ def upsert_company_config(schema, table, **kwargs):
         # inserting new data to target
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id}, id, "name", human_name, value, now() as inserted_at
+            SELECT {comp_id}, id, "name", human_name, value, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
         '''
         cursor.execute(query)
@@ -237,7 +237,7 @@ def upsert_discounts(schema, table, date_column, **kwargs):
                 is_ongoing, happy_weekdays, min_subtotal_price, uses_count, is_once_per_patient, 
                 bogo_buy, bogo_get, bogo_multiple, is_first_time_patient, is_show_promo_code_on_potify, 
                 max_subtotal_price, display_name, is_show_name_on_collection_tile, image, tv_image, 
-                product_filter_id, created_at, updated_at, hide_banner, display_priority, now() as inserted_at
+                product_filter_id, created_at, updated_at, hide_banner, display_priority, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -289,7 +289,7 @@ def upsert_patient_group_ref(schema, table, date_column, **kwargs):
         # inserting increment to target table
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id}, id, patient_id, group_id, sync_created_at, sync_updated_at, now() as inserted_at
+            SELECT {comp_id}, id, patient_id, group_id, sync_created_at, sync_updated_at, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -341,7 +341,7 @@ def upsert_patient_group(schema, table, date_column, **kwargs):
         # inserting increment to target table
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id}, id, "name", sync_created_at, sync_updated_at, is_auto, start_date, end_date, now() as inserted_at
+            SELECT {comp_id}, id, "name", sync_created_at, sync_updated_at, is_auto, start_date, end_date, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -412,7 +412,7 @@ def upsert_patients(schema, table, date_column, **kwargs):
                 pat_city_id, pat_zip_id, is_typist_work, pat_dmv_alt, pat_is_blacklist, pat_blacklist_reason, "type", pat_phone_twilio_lookup, 
                 pat_alt_phone_twilio_lookup, is_twilio_lookup, agile_crm_id, potify_id, potify_earned_cashback, potify_spent_cashback, created_at, 
                 updated_at, is_tax_exempt, tax_profile_id, phone_is_consented, email_is_consented, phone_consent_given_at, 
-                phone_consent_signature, email_consent_given_at, email_consent_signature, deleted_at, tax_tier_id, buyer_status, marketplace, now() as inserted_at
+                phone_consent_signature, email_consent_given_at, email_consent_signature, deleted_at, tax_tier_id, buyer_status, marketplace, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -452,7 +452,7 @@ def upsert_product_categories(schema, table, **kwargs):
             SELECT {comp_id}, id, "name", description, photo, lft, rgt, 
                 "level", created_at, updated_at, is_system, sync_updated_at, 
                 sync_created_at, icon_name, activation_time, 
-                label_template_internal_id, icon_color, system_id, now() as inserted_at
+                label_template_internal_id, icon_color, system_id, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
         '''
         cursor.execute(query)
@@ -483,7 +483,7 @@ def upsert_product_filter_index(schema, table, **kwargs):
         # inserting new data to target
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id}, id, product_id, product_filter_id, now() as inserted_at
+            SELECT {comp_id}, id, product_id, product_filter_id, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
         '''
         cursor.execute(query)
@@ -512,7 +512,7 @@ def upsert_product_transactions(schema, table, date_column, **kwargs):
                 qty, price, price_per, total_price, date, note, item_type, 
                 transfer_direction, qty_free, product_checkin_id, product_name, 
                 office_to_id, product_to_id, product_to_name, cost, order_id, 
-                base_weight, product_checkin_to_id, office_name, now() as inserted_at
+                base_weight, product_checkin_to_id, office_name, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
             WHERE {date_column} > (
                 select coalesce(max({date_column}), '1970-01-01 00:00:00'::timestamp)
@@ -565,7 +565,7 @@ def upsert_product_vendors(schema, table, date_column, **kwargs):
             INSERT INTO {schema}.{table}
             SELECT {comp_id}, id, name, description, phone, email, created_at, updated_at, patient_id, address, 
                 fax, skype, balance, sync_updated_at, sync_created_at, deleted_at, license, metrc_name, 
-                metrc_license, vendor_type, registration_certificate, now() as inserted_at
+                metrc_license, vendor_type, registration_certificate, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -638,7 +638,7 @@ def upsert_products(schema, table, date_column, **kwargs):
                 marijuana_product_type, prod_is_excise, is_metrc, is_metrc_compliant, is_only_each, net_weight, net_weight_measure, show_on_leafly, 
                 is_preroll, preroll_weight, product_type_id, prod_upc, is_tax_exempt, cast(is_under_package_control as boolean) as is_under_package_control, 
                 directions, twcc, product_class, brand_id, potify_brand_product_id, wm_product_id, brand_product_strain_name, 
-                prod_tax_tier_version_id, is_city_local_tax_exempt, brutto_weight, brutto_weight_validation, custom_cost, now() as inserted_at
+                prod_tax_tier_version_id, is_city_local_tax_exempt, brutto_weight, brutto_weight_validation, custom_cost, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -672,7 +672,7 @@ def upsert_register_log(schema, table, date_column, **kwargs):
                 over_drawer, created_at, updated_at, "type", service_history_id, sf_guard_user_id, amount, register_type, 
                 total_cost, total_profit, discount, tax, total_amount, method1_amount, method2_amount, method3_amount, 
                 method4_amount, method5_amount, method6_amount, method7_amount, cash_returns, delivered_amount, pending_amount, 
-                dc_cash_change, vehicle_id, now() as inserted_at
+                dc_cash_change, vehicle_id, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
             WHERE {date_column} > (
                 select coalesce(max({date_column}), '1970-01-01 00:00:00'::timestamp)
@@ -731,7 +731,7 @@ def upsert_register(schema, table, date_column, **kwargs):
                 all_methods_total::real, port, ip_address, sync_updated_at, sync_created_at, office_id, push_new_patient, 
                 push_patient_approved, push_patient_declined, push_new_order, push_order_delivered, platform, tip_amount, 
                 poshub_id, eta, eta_updated_at, application_name, application_version, signin_sf_guard_user_id, dc_cash_change, 
-                dispatch_orders_based_on_delivery_zones, vehicle_id, now() as inserted_at
+                dispatch_orders_based_on_delivery_zones, vehicle_id, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -788,7 +788,7 @@ def upsert_tax_payment(schema, table, date_column, **kwargs):
                 created_at, updated_at, is_deleted, state_sales_tax, county_sales_tax, 
                 city_sales_tax, state_local_tax, county_local_tax, city_local_tax, 
                 excise_tax, state_delivery_sales_tax, county_delivery_sales_tax, 
-                city_delivery_sales_tax, city_delivery_local_tax, excise_delivery_tax, now() as inserted_at
+                city_delivery_sales_tax, city_delivery_local_tax, excise_delivery_tax, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -855,7 +855,7 @@ def upsert_warehouse_orders(schema, table, date_column, **kwargs):
                 patient_has_caregiver, patient_is_tax_exempt, metrc_substatus, checkout_staff_id, pos_mode, signature, 
                 delivery_method, courier_number, patient_rec_number, office_zip_name, refund_type, returned_at, 
                 shipping_method_name, tax_tier_version_id, vehicle, metrc_delivery_status, resend_staff_id, 
-                delivery_estimated_time_of_arrival, now() as inserted_at
+                delivery_estimated_time_of_arrival, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -917,7 +917,7 @@ def upsert_warehouse_order_items(schema, table, date_column, **kwargs):
                 discount_type, free_amount, paid_amount, wcii_cart_item, sync_created_at, sync_updated_at, 
                 product_checkin_id, is_excise, returned_at, is_marijuana_product, product_is_tax_exempt, 
                 is_metrc, is_under_package_control, base_amount, discount_id, delivery_tax, discount_count, 
-                is_exchanged, exchanged_at, product_brutto_weight, product_brutto_weight_validation, confirmed_at, now() as inserted_at
+                is_exchanged, exchanged_at, product_brutto_weight, product_brutto_weight_validation, confirmed_at, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -975,7 +975,7 @@ def upsert_service_history(schema, table, date_column, **kwargs):
                 has_exam_for_approving, order_id, amount_by_referral_points, count_referral_points, register_id, 
                 balance, method1_amount, method2_amount, method3_amount, method4_amount, method5_amount, method6_amount, 
                 method7_amount, profit, is_take_payment, tax, dc_cash_change, is_dejavoo_payment, "type", cost, discount, 
-                "free", payment_method, payment_amount, now() as inserted_at
+                "free", payment_method, payment_amount, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -1033,7 +1033,7 @@ def upsert_product_checkins(schema, table, date_column, **kwargs):
                 is_metrc, available_qty, is_finished, producer_id, vendor_type, vendor_name, 
                 facility_id, is_special, packaged_date, best_by_date, deleted_at, production_run, 
                 is_under_package_control, is_form_modified, metrc_initial_quantity, external_barcode, 
-                packaged_by_id, manifest, is_sample_package, paused_to_datetime, excise_tax_paid, now() as inserted_at
+                packaged_by_id, manifest, is_sample_package, paused_to_datetime, excise_tax_paid, current_timestamp as inserted_at
             FROM {table}_{comp_id}_temp
         '''
         cursor.execute(query)
@@ -1117,7 +1117,7 @@ def upsert_warehouse_order_logs(schema, table, date_column, **kwargs):
         # inserting new data with increment to target
         query = f'''
             INSERT INTO {schema}.{table}
-            SELECT {comp_id} as comp_id, id, order_id, "type", sf_guard_user_id, order_courier_register_id, created_at, register_id, application, now() as inserted_at
+            SELECT {comp_id} as comp_id, id, order_id, "type", sf_guard_user_id, order_courier_register_id, created_at, register_id, application, current_timestamp as inserted_at
             FROM {ext_schema}.{table}
             WHERE
                 {date_column} > (
