e55e59c74665a5ad88e5947223fc6fe0702797e4
“Simarglok” <simarglok@gmail.com>
2023-03-02 18:39:03 +0400

    LOOK-220 added func as exclude: upsert_warehouse_order_items

--- update_everything_new_dev.py
+++ update_everything_new_dev.py
@@ -121,6 +121,97 @@ def get_customers(ti=None, **kwargs):
     ti.xcom_push(key='customers', value=data)
 
 
+@task
+def upsert_warehouse_order_items(schema, table, date_column, **kwargs):
+    ti, task_id = kwargs['ti'], kwargs['task'].task_id
+    customers = ti.xcom_pull(key='customers', task_ids='get_customers')
+    # get max_comp_id from target table and filter list of customers
+    max_comp_id = int(Variable.get(task_id, 0))
+    customers = [c for c in customers if c[0] > max_comp_id]
+    # check if table not exists
+    query = f'''
+        select 1
+        from information_schema.tables
+        where table_schema = '{schema}' and table_name = '{table}'
+        '''
+    cursor.execute(query)
+    table_exists = cursor.fetchone()
+    logging.info(f'Table exists value: {table_exists}')
+    if table_exists is None:
+        # create blank table
+        comp_id = customers[0][0]
+        ext_schema = customers[0][1]
+        query = f'''
+            create table {schema}.{table} as
+            select {comp_id} as comp_id, id, order_id, product_id, "name", descr, price_type, price_per, 
+                charge_by, price, qty, qty_free, amount, tax, discount_value, discount_type_bak, total_amount, 
+                created_at, updated_at, is_charge_by_order, is_free, free_discount, income, discount_amount, 
+                item_type, count, special_id, special_item_id, is_half_eighth, is_returned, returned_amount, 
+                discount_type, free_amount, paid_amount, wcii_cart_item, sync_created_at, sync_updated_at, 
+                product_checkin_id, is_excise, returned_at, is_marijuana_product, product_is_tax_exempt, 
+                is_metrc, is_under_package_control, base_amount, discount_id, delivery_tax, discount_count, 
+                is_exchanged, exchanged_at, product_brutto_weight, product_brutto_weight_validation, current_timestamp as confirmed_at, current_timestamp as inserted_at
+            from {ext_schema}.{table}
+            where false
+            '''
+        cursor.execute(query)
+        redshift_conn.commit()
+        logging.info(f'Table {schema}.{table} created successfully')
+    for comp_id, ext_schema in customers:
+        logging.info(f'Task is starting for company {comp_id}')
+        # creating temp table with new data increment
+        query = f'''
+            CREATE temporary TABLE {table}_{comp_id}_temp as
+            SELECT {table}.*, warehouse_orders.confirmed_at
+            FROM {ext_schema}.{table}
+            INNER JOIN {ext_schema}.warehouse_orders
+            ON {table}.order_id = warehouse_orders.id
+            WHERE {table}.{date_column} > (
+                SELECT coalesce(max({date_column}), '1970-01-01 00:00:00'::timestamp)
+                FROM {schema}.{table}
+                WHERE comp_id = {comp_id}
+            ) and {table}.{date_column} < CURRENT_DATE + interval '8 hours'
+                and warehouse_orders.confirmed_at IS NOT NULL
+        '''
+        cursor.execute(query)
+        logging.info(f'Temp table is created')
+        # deleting from target table data that were updated
+        query = f'''
+            DELETE FROM {schema}.{table}
+            USING {table}_{comp_id}_temp
+            WHERE {schema}.{table}.comp_id = {comp_id}
+                AND {schema}.{table}.id = {table}_{comp_id}_temp.id
+        '''
+        cursor.execute(query)
+        logging.info(f'{cursor.rowcount} rows deleted for {comp_id} at {datetime.now()}')
+        # inserting increment to target table
+        query = f'''
+            INSERT INTO {schema}.{table}
+            SELECT {comp_id} as comp_id, id, order_id, product_id, "name", descr, price_type, price_per, 
+                charge_by, price, qty, qty_free, amount, tax, discount_value, discount_type_bak, total_amount, 
+                created_at, updated_at, is_charge_by_order, is_free, free_discount, income, discount_amount, 
+                item_type, count, special_id, special_item_id, is_half_eighth, is_returned, returned_amount, 
+                discount_type, free_amount, paid_amount, wcii_cart_item, sync_created_at, sync_updated_at, 
+                product_checkin_id, is_excise, returned_at, is_marijuana_product, product_is_tax_exempt, 
+                is_metrc, is_under_package_control, base_amount, discount_id, delivery_tax, discount_count, 
+                is_exchanged, exchanged_at, product_brutto_weight, product_brutto_weight_validation, confirmed_at, current_timestamp as inserted_at
+            FROM {table}_{comp_id}_temp
+        '''
+        cursor.execute(query)
+        logging.info(f'{cursor.rowcount} rows inserted for {comp_id} at {datetime.now()}')
+        # deleting temp table
+        query = f'''
+            DROP TABLE {table}_{comp_id}_temp
+        '''
+        cursor.execute(query)
+        logging.info(f'Temp table is dropped')
+        # commit to target DB
+        redshift_conn.commit()
+        logging.info(f'Task is finished for company {comp_id}')
+        Variable.set(task_id, comp_id)
+    Variable.set(task_id, 0)
+
+
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
@@ -249,11 +340,12 @@ with DAG(
             python_callable=stg_load,
             op_args=['user_activity_record'],
         )
-        warehouse_order_items = PythonOperator(
-            task_id="warehouse_order_items",
-            python_callable=stg_load,
-            op_args=['warehouse_order_items'],
-        )
+        # warehouse_order_items = PythonOperator(
+        #     task_id="warehouse_order_items",
+        #     python_callable=stg_load,
+        #     op_args=['warehouse_order_items'],
+        # )
+        upsert_warehouse_order_items(schema='mock', table='warehouse_order_items', date_column='updated_at')
         warehouse_order_logs = PythonOperator(
             task_id="warehouse_order_logs",
             python_callable=stg_load,
@@ -264,6 +356,7 @@ with DAG(
             python_callable=stg_load,
             op_args=['warehouse_orders'],
         )
+        
 
     # dbt_run = DbtRunOperator(
     #     task_id="dbt_run",
